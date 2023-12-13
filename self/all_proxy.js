b99ecb563f912ae90a332b9fe32d7bb5b38bdc1e
Justin62628 <2410377391@qq.com>
2023-12-13 20:57:07 +0800

    add self switch js

---
+++ self/all_proxy.js
@@ -0,0 +1,20 @@
+/* all_proxy, all_direct, filter*/
+const dict = { "running_mode": "all_proxy" };
+const message = {
+    action: "set_running_mode",
+    content: dict
+};
+
+$configuration.sendMessage(message).then(resolve => {
+    if (resolve.error) {
+        console.log(resolve.error);
+    }
+    if (resolve.ret) {
+        let output=JSON.stringify(resolve.ret);
+        console.log(output);
+    }
+    $done();
+}, reject => {
+    // Normally will never happen.
+    $done();
+});
